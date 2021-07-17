d724dffac952d8c747151aa4dcd6ba0e2b696051
Amit kumar <Amit kumar@DESKTOP-ORHV37K>
2021-07-17 12:01:20 +0530

    First Commit

---
+++ employee_managment/src/employee/managment/conn.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.managment;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Amit Kumar
+ */
+class conn {
+
+    static Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static PreparedStatement PrepareStatement(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static PreparedStatement PrepareStatement(int i, String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
